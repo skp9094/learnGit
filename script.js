c006ff6e151fd5ee5738f476cb1ff94e11ca0299
souravKumar <souravKumar201810@gmail.com>
2023-08-19 12:33:26 +0530

    this is initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// // let box = document.querySelector(".box");
+// // window.addEventListener("scroll",function(e){
+// //     // console.log(window.scrollY)
+// //     box.style.left = window.scrollY  + "px";
+// // });
+
+// console.log(window.open());
